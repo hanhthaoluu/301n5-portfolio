8f9567b55173857789dfbc0836cc4b4135e25b75
hanhthaoluu <hanhthaoluu@gmail.com>
2017-08-19 16:40:32 -0700

    changes to index.html resume.js educationData.json

--- public/scripts/resume.js
+++ public/scripts/resume.js
@@ -10,11 +10,11 @@
       console.log(error);
     });
 
-    // $.getJSON('./data/resumeData.json', function(data) {
-    //   var source = $('#education-template').html();
-    //   var template = Handlebars.compile(source);
-    //   var html = template(data);
-    //   $('#school').append(html);
-    // }).fail(function(res, text, error){
-    //   console.log(error);
-    // });
+    $.getJSON('./data/educationData.json', function(data) {
+      $.each(data, function(index, value) {
+        var source = $('#education-template').html();
+        var template = Handlebars.compile(source);
+        var html = template(value);
+        $('#schools').append(html);
+      });
+    });
