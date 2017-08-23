e931d189a24b4d988eeb08d91f93c514e08b9629
hanhthaoluu <hanhthaoluu@gmail.com>
2017-08-22 21:16:53 -0700

    debugged seattleOutdoors.json and .js and resized pictures

--- public/scripts/outdoors.js
+++ public/scripts/outdoors.js
@@ -1,35 +1,35 @@
-'use strict';
-
-var articles = [];
-
-function Article (rawDataObj) {
-  this.place = rawDataObj.place;
-  this.picture = rawDataObj.picture;
-  this.activities = rawDataObj.activities;
-  this.address = rawDataObj.address;
-  this.website = rawDataObj.website;
-}
-
-Article.prototype.toHtml = function() {
-  var source = $("#article-template").html();
-  var template = Handlebars.compile(source);
-  var context = {Place: this.place, Picture: this.picture, Activities: this.activities, Address: this.address, Website: this.website};
-  var html = template(context);
-  return html;
-};
-
-rawData.forEach(function(articleObject) {
-  articles.push(new Article(articleObject));
-});
-
-articles.forEach(function(article) {
-  $('#content-seattleOutdoors').append(article.toHtml());
-});
-
-Article.fetchAll = function() {
-  if (!localStorage.rawData) {
-    $.getJSON('./scripts/seattleOutdoors.json', function(data) {
-      localStorage.setItem('rawData', JSON.stringify(data));
-    });
-  }
-}
+// 'use strict';
+//
+// var articles = [];
+//
+// function Article (rawDataObj) {
+//   this.place = rawDataObj.place;
+//   this.picture = rawDataObj.picture;
+//   this.activities = rawDataObj.activities;
+//   this.address = rawDataObj.address;
+//   this.website = rawDataObj.website;
+// }
+//
+// Article.prototype.toHtml = function() {
+//   var source = $("#article-template").html();
+//   var template = Handlebars.compile(source);
+//   var context = {Place: this.place, Picture: this.picture, Activities: this.activities, Address: this.address, Website: this.website};
+//   var html = template(context);
+//   return html;
+// };
+//
+// rawData.forEach(function(articleObject) {
+//   articles.push(new Article(articleObject));
+// });
+//
+// articles.forEach(function(article) {
+//   $('#content-seattleOutdoors').append(article.toHtml());
+// });
+//
+// Article.fetchAll = function() {
+//   if (!localStorage.rawData) {
+//     $.getJSON('./scripts/seattleOutdoors.json', function(data) {
+//       localStorage.setItem('rawData', JSON.stringify(data));
+//     });
+//   }
+// }
--- public/scripts/seattleOutdoors.js
+++ public/scripts/seattleOutdoors.js
@@ -1,9 +1,11 @@
 
 $.getJSON('./data/seattleOutdoors.json', function(data) {
   $.each(data, function(index, value) {
-    var source = $('#education-template').html();
+    var source = $('#article-template').html();
     var template = Handlebars.compile(source);
     var html = template(value);
     $('#content-seattleOutdoors').append(html);
   });
+}).fail(function(res, text, error){
+  console.log(error);
 });
