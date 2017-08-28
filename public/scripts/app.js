894371d0c03ac9c3641c1f872df0c0f24d13e0d4
hanhthaoluu <hanhthaoluu@gmail.com>
2017-08-28 14:37:54 -0700

    wrapped all functions inside iffe functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,32 @@
 'use strict';
-function projectData (projectName, path, description) {
-  this.projectName = name;
-  this.path = path;
-  this.description = description;
-}
 
-$(document).ready(function(){
-  $('.content').children().not('#content-home').hide();
+var app = app || {};
 
-});
+(function() {
+  function projectData (projectName, path, description) {
+    this.projectName = name;
+    this.path = path;
+    this.description = description;
+  }
 
-$('.main-nav').on('mouseover', function() {
-  $(this).css("background-color", "grey");
-});
+  $(document).ready(function(){
+    $('.content').children().not('#content-home').hide();
+  });
 
-$('.icon-menu').click(function() {
-  var $navList = $('.main-nav>ul');
-  $navList.attr('id') === 'hidden' ? $navList.attr('id', '') : $navList.attr('id', 'hidden');
-});
+  $('.main-nav').on('mouseover', function() {
+    $(this).css("background-color", "grey");
+  });
 
-$('.main-nav ul li').click(function(e) {
-  e.preventDefault();
-  $('.content').children().hide();
-  $(`#content-${$(this).attr('id')}`).show();
-  //backtick character is for string template to avoid doing string concatenation
-  //$(`#${$(this).attr('id')}`).show();
+  $('.icon-menu').click(function() {
+    var $navList = $('.main-nav>ul');
+    $navList.attr('id') === 'hidden' ? $navList.attr('id', '') : $navList.attr('id', 'hidden');
+  });
 
-});
+  $('.main-nav ul li').click(function(e) {
+    e.preventDefault();
+    $('.content').children().hide();
+    $(`#content-${$(this).attr('id')}`).show();
+    //backtick character is for string template to avoid doing string concatenation
+    //$(`#${$(this).attr('id')}`).show();
+  });
+})(app);
