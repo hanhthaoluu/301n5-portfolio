2145cba4aa8d95d331347ccc2924aa0c641c946b
hanhthaoluu <hanhthaoluu@gmail.com>
2017-09-04 00:24:17 -0700

    deleted notes

--- public/scripts/views/seattleOutdoorsView.js
+++ public/scripts/views/seattleOutdoorsView.js
@@ -7,16 +7,10 @@ var app = app || {};
 
 
   seattleOutdoorsView.view = function() {
-    //$('#content-seattleOutdoors').html('');
     var htmlAll = app.SeattleOutdoors.all.map(function(value) {
       var source = $('#article-template').html();
-      console.log('hi source: ' + source);
-      ////grab from index.html everything inside the Handlebars template named "article-template"
       var template = Handlebars.compile(source);
-      console.log('hi template ' + template);
-      //var html = template(value);
-      return template(value); //return the actual html
-      // $('#content-seattleOutdoors').append(html);
+      return template(value);
     });
 
     var result = htmlAll.reduce(function(accumulator, currentValue) {
