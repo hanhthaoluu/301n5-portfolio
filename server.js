e0029b5ff2640c6b9f8ea5ad6bf7e2d99b56efd9
hanhthaoluu <hanhthaoluu@gmail.com>
2017-09-04 13:19:52 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
 'use strict';
 
-const pg = require('pg');
 const express = require('express');
 const sa = require('superagent');
 const app = express();
 const PORT  = process.env.PORT || 3000;
-const conString = process.env.DATABASE_URL || '';
-const client = new pg.Client(conString);
-client.connect();
-client.on('error', err => console.log(err));
+
 app.use(express.static('./public'));
 
 app.listen(PORT, function() {
   console.log(`you are hosted on port ${PORT}`);
 });
 
-app.get('/repos', (req, res) => {
-  sa.get('https://api.github.com/user/repos')
-    .set('Authorization', `token ${process.env.GITHUB_TOKEN}`)
-    .end((err, result) => {
-      if(err) console.log(err);
-      res.send(result.body);
-    });
-});
+// app.get('/repos', (req, res) => {
+//   sa.get('https://api.github.com/user/repos')
+//     .set('Authorization', `token ${process.env.GITHUB_TOKEN}`)
+//     .end((err, result) => {
+//       if(err) console.log(err);
+//       res.send(result.body);
+//     });
+// });
 
 app.set('/*', (request, response) => {
   response.sendFile('index.html', {root: './public'});
